c735af7e91831690215245ebf7f7b71b50013301
marcelofb <marceloburriel@gmail.com>
2015-11-25 00:37:31 -0300

    Se muestra en la barra de navegación el usuario logueado

--- app/scripts/controllers/LoginController.js
+++ app/scripts/controllers/LoginController.js
@@ -4,21 +4,32 @@
 
     angular
         .module("webApp.controllers")
-        .controller("LoginController", LoginController);
+        .controller("LoginController", LoginController)
+        .factory("Auth", Auth);
 
-    LoginController.$inject = ["$scope"];
+    LoginController.$inject = ["$scope", "Auth"];
+    Auth.$inject = ["$firebaseAuth"];
 
-    function LoginController($scope, $firebaseArray, $location, $firebaseObject) {
+    function LoginController($scope, Auth) {
         //login con Facebook
         $scope.log = function () {
             var fb = new Firebase("https://voting-web.firebaseio.com");
+            $scope.auth = Auth;
             fb.authWithOAuthPopup("facebook", function (error, authData) {
                 if (error) {
                     console.log("Login Failed!", error);
                 } else {
                     console.log("Authenticated successfully with payload:", authData);
+                    $scope.auth.$onAuth(function(authData) {
+                        $scope.authData = authData;
+                    });
                 }
             });
         }
+    }
+
+    function Auth($firebaseAuth) {
+        var ref = new Firebase("https://voting-web.firebaseio.com");
+        return $firebaseAuth(ref);
     }
 }());
