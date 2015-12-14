3cba899951312ca90846e6b5d3ce2639b1cb0184
Leandro <leito.vm3@hotmail.com>
2015-12-14 17:42:45 -0300

    Tratando de recuperar el usuario por el id, llega una referencia al objeto, todavia no esta completo

--- app/scripts/factories/UsersFactory.js
+++ app/scripts/factories/UsersFactory.js
@@ -12,7 +12,7 @@
 
     function UsersFactory($firebaseArray, $firebaseObject) {
 
-        var firebaseConnectionUrl = "https://voting-web.firebaseio.com/Users/";
+        var firebaseConnectionUrl = "https://voting-web.firebaseio.com/Users";
 
         var service = {
             createOrRetrieveUser: createOrRetrieveUser
@@ -35,34 +35,33 @@
             var ref = getFirebaseObj();
 
             return new Promise(function (resolve, reject) {
-                ref.orderByChild('facebookId').startAt(facebookUser.id).endAt(facebookUser.id).once('value', function (result) {
-                    var retrievedUser = result.val();
+                //ref.orderByChild('facebookId').startAt(facebookUser.id).endAt(facebookUser.id).once('value', function (result) {
+                    //var retrievedUser = result.val();
+                var retrievedUser = ref.child(facebookUser.id);
 
                     if (utils.isEmpty(retrievedUser)) {
                         console.log("$$$ El usuario no existe en la base, se crea uno nuevo");
                         var syncedUsers = $firebaseArray(ref),
                             newUser = {
-                                facebookId: facebookUser.id,
                                 name: facebookUser.displayName,
                                 image: facebookUser.profileImageURL
                             };
 
-                        syncedUsers.$add(newUser).then(function (result) {
-                            console.log('$$$ Se guardo el usuario :)');
-                            resolve(newUser);
-                        }, function (error) {
-                            reject(error);
+                        ref.child(facebookUser.id).set(newUser, function (error) {
+                            if(error) {
+                               reject(error);
+                            } else {
+                                var facebookId = facebookUser.id;
+                                console.log('$$$ Se guardo el usuario :)');
+                                resolve({ facebookId : newUser });
+                            }
                         });
+
                     } else {
                         console.log("El usuario ya existe en la base!", retrievedUser);
-                        // Esto es una bazofia, pero firebase devuelve un objeto { Kyrk123145: Object }
-                        // donde el nro raro es el id que usa y el Object es el objeto posta con los
-                        // datos del usuario. Si lo necesitamos ese ID podemos cambiar este return
-                        // El código recupera ese Object, el value.
-                        var user = retrievedUser[Object.keys(retrievedUser)[0]];
-                        resolve(user);
+                        resolve(retrievedUser);
                     }
-                });
+                //});
             });
         }
     }
