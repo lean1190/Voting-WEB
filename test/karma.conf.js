f136d6e5ce0b2c493d3eed2a43abb38a8004c1b0
marcelofb <marceloburriel@gmail.com>
2016-01-06 20:36:35 -0300

    corrección de error en merge

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -39,6 +39,7 @@ module.exports = function(config) {
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-local-storage/dist/angular-local-storage.js',
+      'bower_components/angularUtils-pagination/dirPagination.js',
       'bower_components/later/later.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
